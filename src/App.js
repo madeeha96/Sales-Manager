978d936311f0ec0e991ab4e0830b466ff456e166
Hammad Tariq <hammadtariq65@gmail.com>
2017-11-06 18:26:47 +0500

    Signup component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Signup from './Signup';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: []
+    }
+  }
+
+  getUser(user) {
+    this.setState({user: this.state.user.push(user)});
+    console.log("my users", this.state.user);
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +26,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Signup getUser={this.getUser.bind(this)}/>
       </div>
     );
   }
